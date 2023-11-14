e3caacb108d72de298bf1f5e9e56ef785dbf6994
Canny <promiseokpoto5050@gmail.com>
2023-11-14 10:51:18 +0100

    last changes

---
+++ src/Components/ServicesData.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import PHOTOS from './images/index';
+
+
+const Data = [
+    {
+        id: 1,
+        image: PHOTOS.PHOTO13,
+        heading: "Photovoltaic (PV) Solar Power",
+        paragraph: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Similique veritatis consequuntur nemo inventore eum esse dolor fugiat minus reiciendis ab veniam nulla accusantium necessitatibus voluptatibus rerum, ducimus, tempora tenetur eaque."
+    },
+
+    {
+        id: 2,
+        image: PHOTOS.PHOTO14,
+        heading: "Solar Thermal Energy",
+        paragraph: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Similique veritatis consequuntur nemo inventore eum esse dolor fugiat minus reiciendis ab veniam nulla accusantium necessitatibus voluptatibus rerum, ducimus, tempora tenetur eaque."
+    },
+
+    {
+        id: 3,
+        image: PHOTOS.PHOTO25,
+        heading: "Solar Lighting",
+        paragraph: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Similique veritatis consequuntur nemo inventore eum esse dolor fugiat minus reiciendis ab veniam nulla accusantium necessitatibus voluptatibus rerum, ducimus, tempora tenetur eaque."
+    },
+
+    {
+        id: 4,
+        image: PHOTOS.PHOTO13,
+        heading: "Solar Architecture",
+        paragraph: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Similique veritatis consequuntur nemo inventore eum esse dolor fugiat minus reiciendis ab veniam nulla accusantium necessitatibus voluptatibus rerum, ducimus, tempora tenetur eaque."
+    },
+
+    {
+        id: 5,
+        image: PHOTOS.PHOTO14,
+        heading: "training",
+        paragraph: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Similique veritatis consequuntur nemo inventore eum esse dolor fugiat minus reiciendis ab veniam nulla accusantium necessitatibus voluptatibus rerum, ducimus, tempora tenetur eaque."
+    },
+]
+
+
+export default Data;
