d45655ba9b4875824735d965bbb0dce09a3f0fd0
Canny <promiseokpoto5050@gmail.com>
2023-11-30 13:12:23 +0100

    my changes

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -6,24 +6,46 @@ import { NavLink } from 'react-router-dom';
 function Hero(){
 
         return(
-            <div className="hero-section">
-                <div className="left-hand-side">
+            <div className="hero">
+                <div className="hero-section">
+                    <div className="left-hand-side">
+                        <h2>NEW ARRIVALS ONLY</h2>
+                        <div className="left-sub">
+                            <div className="hand">
+                                <p>new</p>
+                                <img src={PHOTOS.WAVE} alt=""/>
+                            </div>
+                            <p className="collect">collections for everyone</p>
+                        </div>
+                        <NavLink to="/" className='latest-btn'>
+                            <div>Latest Collections</div>
+                            <i class="fa-solid fa-arrow-right"></i>
+                        </NavLink>
+                    </div>
+            
+                    <div className="right-hand-side">
+                        <img src={PHOTOS.HERO} alt=""/>
+                    </div>
+                </div>
+
+                <div className="hero-section2">
+                    <div className="hero-wrapper">
                     <h2>NEW ARRIVALS ONLY</h2>
-                    <div className="left-sub">
-                        <div className="hand">
+
+                        <div className="top-sub">
                             <p>new</p>
                             <img src={PHOTOS.WAVE} alt=""/>
                         </div>
+
                         <p className="collect">collections for everyone</p>
+
+                        <img src={PHOTOS.HERO} alt=""/>
+
+                        <NavLink to="/" className='latest-btn'>
+                            <div>Latest Collections</div>
+                            <i class="fa-solid fa-arrow-right"></i>
+                        </NavLink>
                     </div>
-                    <NavLink to="/" className='latest-btn'>
-                        <div>Latest Collections</div>
-                        <i class="fa-solid fa-arrow-right"></i>
-                    </NavLink>
-                </div>
-        
-                <div className="right-hand-side">
-                    <img src={PHOTOS.HERO} alt=""/>
                 </div>
             </div>
         )
